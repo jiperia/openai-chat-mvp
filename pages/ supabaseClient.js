b80415537d5ec13d4f3aca1e138c2c6b5cd35a5f
jiperia <max@aufbruch-ki.de>
2025-09-26 22:21:43 +0200

    Delete pages/ supabaseClient.js

--- pages/ supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Hier DEINE echten Werte eintragen!
-const SUPABASE_URL = "https://kxyambnweumzcrhubkvd.supabase.co"; // Deine Supabase-Projekt-URL
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4eWFtYm53ZXVtemNyaHVia3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5MTYxMjgsImV4cCI6MjA3NDQ5MjEyOH0.8z_2_A-t0THsoT8j0qv1nfUFMEXgzeyJrUtUMMAPq8c"; // Dein Public API Key (anon), aus Dashboard/API
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
